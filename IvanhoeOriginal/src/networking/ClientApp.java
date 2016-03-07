9ee6ce41afa212bf2074896010ba3bf453dad3e0
Aziza Magerramova <aziza784@gmail.com>
2016-03-07 03:12:47 -0500

    now I have implemented the respective functions to make my tests pass now all test for 2 players connecting, three players connecting and establishing connection

--- IvanhoeOriginal/src/networking/ClientApp.java
+++ IvanhoeOriginal/src/networking/ClientApp.java
@@ -1,8 +1,26 @@
 package networking;
 
-public class ClientApp {
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class ClientApp {
+	
+	private Socket socket = null;
+	
 	public ClientApp(String serverName, int serverPort, String pName) {
 		
+		System.out.println("Establishing connection. Please wait ...");
+
+		try {  
+			this.socket = new Socket(serverName, serverPort);
+
+		} catch(UnknownHostException uhe) {  
+			System.err.println(": Unknown Host");
+			System.out.println(uhe.getMessage());
+		} catch(IOException ioe) {  
+			System.out.println(": Unexpected exception");
+			System.out.println(ioe.getMessage());  
+		}
 	}
 }
--- IvanhoeOriginal/src/networking/ServerApp.java
+++ IvanhoeOriginal/src/networking/ServerApp.java
@@ -1,11 +1,24 @@
 package networking;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.HashMap;
+
 public class ServerApp {
 	
-	public boolean connected = false;
+	public boolean connected    = false;
+	private ServerSocket server = null;
 	
 	public ServerApp(int port) {
 		
-	}
+		try {
 
+			/** Establish the servers main port that it will listen on */
+			server    = new ServerSocket(port);
+			connected = true;
+			
+		} catch (IOException ioe) {
+			System.out.println(ioe.getMessage());
+		}
+	}
 }
