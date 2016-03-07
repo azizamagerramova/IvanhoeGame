dafc5dde4ccdd12f45c99e52d32578173ff7f1b9
CHRISTIAN MICHAEL <chukwudiimichael@yahoo.ca>
2016-03-07 04:09:02 -0500

    added couple of classes to make my fucntion for three players connecting and receiving message from server work but it doesnt now but I am still coming with more functions

--- IvanhoeOriginal/src/networking/ClientApp.java
+++ IvanhoeOriginal/src/networking/ClientApp.java
@@ -1,13 +1,21 @@
 package networking;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class ClientApp {
 	
-	private Socket socket = null;
-	private int    ID     = 0;
+	private Socket socket 			 = null;
+	private int    ID     			 = 0;
+	private Thread thread            = null;
+	private ClientThread   client    = null;
+	private BufferedReader console   = null;
+	private BufferedReader streamIn  = null;
+	private BufferedWriter streamOut = null;
+	
 	
 	public ClientApp(String serverName, int serverPort, String pName) {
 		
@@ -32,5 +40,28 @@ public class ClientApp {
 	
 	public int getID() {
 		return ID;
+	}
+	
+	public void handle(String msg) {
+		
+	}
+	
+	public void stop() {  
+		try { 
+			if (thread != null) thread = null;
+			if (console != null) console.close();
+			if (streamIn != null) streamIn.close();
+			if (streamOut != null) streamOut.close();
+
+			if (socket != null) socket.close();
+
+			this.socket = null;
+			this.console = null;
+			this.streamIn = null;
+			this.streamOut = null;    	  
+		} catch(IOException ioe) {  
+			System.out.println(ioe.getMessage());  
+		}
+		client.close();  
 	}
 }
